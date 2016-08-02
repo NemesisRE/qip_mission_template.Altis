43e21e030b2390a296f31292f48d6409e8b29122
NemsisRE <nemesissre@gmail.com>
2016-08-02 02:11:19 +0200

    corrected param values

--- qipTPL/CfgFiles/CfgParams.hpp
+++ qipTPL/CfgFiles/CfgParams.hpp
@@ -9,9 +9,9 @@ class ace_medical_level {
 class ace_medical_useLocation_PAK {
 	title = "$STR_ACE_Medical_AdvancedMedicalSettings_useCondition_PAK_Description";
 	ACE_setting = 1;
-	values[] = {1, 2, 3, 4, 5};
+	values[] = {0, 1, 2, 3, 4};
 	texts[] =  {"$STR_ACE_Medical_AdvancedMedicalSettings_anywhere", "$STR_ACE_Medical_AdvancedMedicalSettings_vehicle", "$STR_ACE_Medical_AdvancedMedicalSettings_facility", "$STR_ACE_Medical_AdvancedMedicalSettings_vehicleAndFacility", "$STR_ACE_Common_Disabled"};
-	default = 1;
+	default = 0;
 };
 
 class ace_map_BFT_ShowPlayerNames {
