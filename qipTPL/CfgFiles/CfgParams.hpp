c0ff9e6a2d4e501ce6cb6b64b110a972e9bb1343
NemsisRE <nemesissre@gmail.com>
2019-07-15 16:04:29 +0000

    removed parameter they are replaced with cba settings

--- qipTPL/CfgFiles/CfgParams.hpp
+++ qipTPL/CfgFiles/CfgParams.hpp
@@ -1,13 +1,1 @@
-class qipTPL {
-	title = "$STR_qipTPL_useTPL";
-	values[] = {0,1};
-	texts[] = {"$STR_qipTPL_Common_Disabled", "$STR_qipTPL_Common_Enabled"};
-	default = 1;
-};
 
-class initTPL {
-	title = "$STR_qipTPL_initTPL";
-	values[] = {0,1};
-	texts[] = {"$STR_qipTPL_Common_Disabled", "$STR_qipTPL_Common_Enabled"};
-	default = 1;
-};
